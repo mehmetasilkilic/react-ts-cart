eee9a028a87788bc3fea9aefde267d1a0c10a36f
mehmetasilkilic <mehmetasil96@gmail.com>
2021-12-28 07:08:47 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./assets/scss/main.scss"
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Drawer from '@mui/material/Drawer';
+import Grid from '@mui/material/Grid';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Badge from '@mui/material/Badge';
 
-function App() {
+
+
+const getProducts = async () => {
+  await (await fetch("https://fakestoreapi.com/products")).json();
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
